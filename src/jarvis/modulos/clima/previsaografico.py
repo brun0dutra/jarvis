f7309f58e260c463169ed88b0c0aac5c9b1c309d
Bruno Dutra <brunodutra1912@gmail.com>
2025-08-09 11:04:59 -0300

    Função previsaografico atualizada, comandos e a função em si, tudo funcionando

--- src/jarvis/modulos/clima/previsaografico.py
+++ src/jarvis/modulos/clima/previsaografico.py
@@ -32,26 +32,26 @@ class Previsaografico(ModuloBase):
             faltando["modo"] = "Para horas ou dias ?"
             faltando["quantidade"] = "Qual o periodo ?"
 
-        if "chuva" in frase:
-            parametros["dado"] = "chuva"
-        elif "volume" in frase:
-            parametros["dado"] = "volume"
-        elif "temperatura" in frase:
-            parametros["dado"] = "temperatura"
-        elif "nuvens" in frase:
-            parametros["dado"] = "nuvens"
-        elif "umidade" in frase:
-            parametros["dado"] = "umidade"
-        elif "vento" in frase:
-            parametros["dado"] = "vento"
-        elif "visibilidade" in frase:
-            parametros["dado"] = "visibilidade"
-        elif "uv" in frase:
-            parametros["dado"] = "uv"
-        elif "orvalho" in frase:
-            parametros["dado"] = "orvalho"
-        else:
-            faltando["dado"] = "Qual informação do clima ?"
+        intencoes = {
+            "temperatura": ["temperatura", "calor", "frio", "sensação", "termica", "maxima", "minima"],
+            "chuva": ["psossibilidade", "precipitação", "probabilidade de chuva", "chuva"],
+            "volume": ["milimetros", "volume"],
+            "nuvens":["nuvem", "nuvens"],
+            "umidade": ["umidade"],
+            "vento": ["vento", "rajada", "direção"],
+            "visibilidade": ["visibilidade", "nevoa", "serração"],
+            "uv": ["uv", "raios uv", "ultravioleta", "raios ultravioleta"],
+            "pressao": ["pressao", "atmosferica"],
+            "orvalho": ["orvalho", "ponto de orvalho"],
+        }
+
+        for chave, palavras in intencoes.items():
+            for p in palavras:
+                if re.search(rf"\b{re.escape(p)}\b", frase):
+                    parametros["dado"] = chave
+                    break
+            if "dado" in parametros:
+                break
 
         return parametros, faltando
 
@@ -75,6 +75,10 @@ class Previsaografico(ModuloBase):
         valores = []
         valoresdois = []
         numero_de_valores = 0
+        nome_valor_um = ""
+        nome_valor_dois = ""
+        cor_valor_um = ""
+        cor_valor_dois = ""
 
         for item in dados_usar:
             labels.append(formatador(item["dt"]))
@@ -84,72 +88,106 @@ class Previsaografico(ModuloBase):
                     valores.append(item["temp"])
                     valoresdois.append(item["feels_like"])
                     numero_de_valores = 2
+                    nome_valor_um = "Temperatura"
+                    nome_valor_dois = "Senssação Termica"
+                    cor_valor_um = "orange"
+                    cor_valor_dois = "green"
                 else:
                     valores.append(item["temp"]["max"]) 
                     valoresdois.append(item["temp"]["min"])
                     numero_de_valores = 2
+                    nome_valor_um = "Maxima"
+                    nome_valor_dois = "Minima"
+                    cor_valor_um = "red"
+                    cor_valor_dois = "blue"
 
             elif dado == "chuva":
                 valores.append(item.get("pop", 0) * 100)
                 numero_de_valores = 1
+                nome_valor_um = "Probabilidade de chuva"
+                cor_valor_um = "blue"
 
             elif dado == "volume":
                 valores.append(item.get("rain", 0))
                 numero_de_valores = 1
+                nome_valor_um = "Milimetros de chuva"
+                cor_valor_um = "blue"
 
             elif dado == "nuvens":
                 valores.append(item["clouds"])
                 numero_de_valores = 1
+                nome_valor_um = "Nuvens"
+                cor_valor_um = "blue"
 
             elif dado == "umidade":
                 valores.append(item["humidity"])
                 numero_de_valores = 1
+                nome_valor_um = "Umidade"
+                cor_valor_um = "blue"
 
             elif dado == "vento":
                 valores.append(item["wind_speed"] * 3.6)
                 valoresdois.append(item.get("wind_gust") * 3.6)
                 numero_de_valores = 2
+                nome_valor_um = "Vento"
+                nome_valor_dois = "Rajadas"
+                cor_valor_um = "blue"
+                cor_valor_dois = "red"
 
             elif dado == "visibilidade":
                 valores.append(item.get("visibility"))
                 numero_de_valores = 1
+                nome_valor_um = "Visibilidade"
+                cor_valor_um = "blue"
 
             elif dado == "uv":
                 valores.append(item.get("uvi", 0))
                 numero_de_valores = 1
+                nome_valor_um = "Raios uv"
+                cor_valor_um = "red"
 
             elif dado == "pressao":
                 valores.append(item.get("pressure", 0))
                 numero_de_valores = 1
+                nome_valor_um = "Pressão Atmosferica"
+                cor_valor_um = "red"
 
             elif dado == "orvalho":
                 valores.append(item.get("dew_point", 0))
                 numero_de_valores = 1
+                nome_valor_um = "Ponto do orvalho"
+                cor_valor_um = "blue"
 
             
 
         # === VISUAL ===
-        plt.style.use('dark_background')
+        plt.style.use('default')  # estilo padrão do matplotlib
         plt.figure(figsize=(12, 5))
-
-        # Cores automáticas por tipo de dado
-        cor = 'orange' if dado == 'temperatura' else 'cyan'
 
         # Plot
         if numero_de_valores == 1:
-            plt.plot(labels, valores, marker='o', linestyle='-', color=cor, linewidth=2)
-        if numero_de_valores == 2:
-            plt.plot(labels, valores, marker='o', linestyle='-', color=cor, linewidth=2)
-            plt.plot(labels, valoresdois, marker='o', linestyle='-', color="orange", linewidth=2)
-        
+            plt.plot(labels, valores, marker='o', linestyle='-', color=cor_valor_um, linewidth=2, label=dado)
+        elif numero_de_valores == 2:
+            plt.plot(labels, valores, marker='o', linestyle='-', color=cor_valor_um, linewidth=2, label=nome_valor_um)
+            plt.plot(labels, valoresdois, marker='o', linestyle='-', color=cor_valor_dois, linewidth=2, label=nome_valor_dois)
 
-        titulo = f"{dado}"
-        plt.title(titulo, fontsize=16, fontweight='bold', color='white')
-        plt.xlabel("Tempo", fontsize=12, color='white')
-        plt.ylabel("°C" if dado == "temperatura" else "%", fontsize=12, color='white')
-        plt.xticks(rotation=45, fontsize=10, color='white')
-        plt.yticks(fontsize=10, color='white')
+        # Título e rótulos (sem cor branca, deixa padrão para o estilo default)
+        plt.title(f"{dado}", fontsize=16, fontweight='bold')
+        plt.xlabel("Tempo", fontsize=12)
+        plt.ylabel("°C" if dado == "temperatura" else "%", fontsize=12)
+        plt.xticks(rotation=45, fontsize=10)
+        plt.yticks(fontsize=10)
         plt.grid(True, linestyle='--', alpha=0.4)
+
+        # Legenda
+        plt.legend()
+
+        # Valores nos pontos
+        for x, y in zip(labels, valores):
+            plt.text(x, y, f"{y:.1f}", ha='center', va='bottom', fontsize=8)
+        if numero_de_valores == 2:
+            for x, y in zip(labels, valoresdois):
+                plt.text(x, y, f"{y:.1f}", ha='center', va='bottom', fontsize=8)
 
         # Bordas mais suaves
         ax = plt.gca()
@@ -158,11 +196,11 @@ class Previsaografico(ModuloBase):
 
         plt.tight_layout()
 
-        # Nome do arquivo com base no dado
+        # Salvar imagem
         nome_arquivo = f"src/jarvis/modulos/clima/graficos/grafico_{dado}_{modo}_{quantidade}.png"
         plt.savefig(nome_arquivo, dpi=100)
         plt.close()
 
-        texto = f"Gráfico de {dado} salvo em: {nome_arquivo}"
+        texto = nome_arquivo
         fala = f"Aqui está o gráfico de {dado} para {quantidade} {modo}."
         return texto, fala
